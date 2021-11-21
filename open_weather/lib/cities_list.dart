feb077ccee916fc804ce869117bda310a52c17cd
rennelou <rennelou@outlook.com>
2021-11-21 15:12:50 -0300

    todos os widgets da tela inicial

--- open_weather/lib/cities_list.dart
+++ open_weather/lib/cities_list.dart
@@ -16,16 +16,12 @@ class _CitiesListState extends State<CitiesList> {
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<List<String>>(
-        stream: searchEngine.stream,
-        initialData: citiesState.toList(),
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            return const Text('Something is wrong');
-          } else {
-            return BuildViewList(snapshot.data!);
-          }
-        });
+    return Column(
+      children: [
+        const Expanded(child: SearchField()),
+        Expanded(child: CitiesListListener(searchEngine, citiesState.toList()))
+      ],
+    );
   }
 
   cacheAppend(String cityName) {
@@ -33,14 +29,48 @@ class _CitiesListState extends State<CitiesList> {
   }
 }
 
-class BuildViewList extends StatelessWidget {
-  final _biggerFont = const TextStyle(fontSize: 18.0);
-  final List<String> cities;
-
-  const BuildViewList(this.cities, {Key? key}) : super(key: key);
+class SearchField extends StatelessWidget {
+  const SearchField({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        const Expanded(child: TextField()),
+        Expanded(
+            child: IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.search),
+        ))
+      ],
+    );
+  }
+}
+
+class CitiesListListener extends StatelessWidget {
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+  final SearchEngineLogic searchEngine;
+  final List<String> initialState;
+
+  const CitiesListListener(this.searchEngine, this.initialState, {Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<List<String>>(
+        stream: searchEngine.stream,
+        initialData: initialState,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return const Text('Something is wrong');
+          } else {
+            return buildViewList(snapshot.data!);
+          }
+        });
+  }
+
+  Widget buildViewList(List<String> cities) {
     return ListView.builder(
         padding: const EdgeInsets.all(16.0),
         itemBuilder: (context, i) {
--- open_weather/lib/main.dart
+++ open_weather/lib/main.dart
@@ -33,9 +33,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
+      appBar: AppBar(title: Text(widget.title)),
       body: const Center(child: CitiesList()),
     );
   }
